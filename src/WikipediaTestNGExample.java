c2e7d60fb729a2c3ee8a89b130cac78f8c9eebb7
Neil <neil@Neils-MacBook-Pro.local>
2015-04-15 12:20:52 -0700

    adding Edit test and removing extra whitespace

--- src/WikipediaTestNGExample.java
+++ src/WikipediaTestNGExample.java
@@ -6,7 +6,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.By;
 
@@ -16,9 +15,9 @@ public class WikipediaTestNGExample {
 
     @BeforeMethod
     public void setUp(Method method) throws Exception {
-        this.driver = new FirefoxDriver();
+    	this.driver = new FirefoxDriver();
     }
-    
+
     @AfterMethod
     public void tearDown() throws Exception {
         driver.quit();
@@ -26,55 +25,54 @@ public class WikipediaTestNGExample {
 
     @Test
     public void verifyLaunch() throws Exception {
-        
-        driver.get("http://wikipedia.org");
-        
+    	driver.get("http://wikipedia.org");
+
         // Make the browser get the page and check its title
         Assert.assertEquals("Wikipedia", driver.getTitle());
-        
+
         // Check if the launch page elements are there
         Assert.assertTrue(driver.findElement(By.cssSelector(".central-featured")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.cssSelector("#searchInput")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.cssSelector("#searchLanguage")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.cssSelector(".search-form .formBtn")).isDisplayed());
     }
-    
+
     @Test
     public void verifySearchForUFC() throws Exception {
-        driver.get("http://wikipedia.org");
-        
-        // Fill out search box with UFC and click search
-        driver.findElement(By.cssSelector("#searchInput")).sendKeys("UFC");
-        driver.findElement(By.cssSelector(".search-form .formBtn")).click();
-        
-        // verify UFC page is there
-        Assert.assertEquals("http://en.wikipedia.org/wiki/Ultimate_Fighting_Championship", driver.getCurrentUrl());
-        Assert.assertEquals("Ultimate Fighting Championship - Wikipedia, the free encyclopedia", driver.getTitle());
+    	driver.get("http://wikipedia.org");
+
+    	// Fill out search box with UFC and click search
+    	driver.findElement(By.cssSelector("#searchInput")).sendKeys("UFC");
+    	driver.findElement(By.cssSelector(".search-form .formBtn")).click();
+
+    	// verify UFC page is there
+    	Assert.assertEquals("http://en.wikipedia.org/wiki/Ultimate_Fighting_Championship", driver.getCurrentUrl());
+    	Assert.assertEquals("Ultimate Fighting Championship - Wikipedia, the free encyclopedia", driver.getTitle());
     }
-    
+
     @Test
     public void goToHistorySection() throws Exception {
-        driver.get("http://en.wikipedia.org/wiki/Ultimate_Fighting_Championship");
-        
-        // click History
-        driver.findElement(By.cssSelector("li [href='#History']")).click();
-        Assert.assertEquals("http://en.wikipedia.org/wiki/Ultimate_Fighting_Championship#History", driver.getCurrentUrl());
-        
-        // make sure we have scrolled to history
-        JavascriptExecutor executor = (JavascriptExecutor) driver;
-        Long value = (Long) executor.executeScript("return window.scrollY;");
-        Assert.assertNotEquals(value, 0, 0);
+    	driver.get("http://en.wikipedia.org/wiki/Ultimate_Fighting_Championship");
+
+    	// click History
+    	driver.findElement(By.cssSelector("li [href='#History']")).click();
+    	Assert.assertEquals("http://en.wikipedia.org/wiki/Ultimate_Fighting_Championship#History", driver.getCurrentUrl());
+
+    	// make sure we have scrolled to history
+    	JavascriptExecutor executor = (JavascriptExecutor) driver;
+    	Long value = (Long) executor.executeScript("return window.scrollY;");
+    	Assert.assertNotEquals(value, 0, 0);
     }
-    
-//    @Test
-//    public void verifyEditPageUI() throws Exception {
-//      driver.get("http://en.wikipedia.org/wiki/Ultimate_Fighting_Championship");
-//      
-//      // click edit page
-//      driver.findElement(By.cssSelector("#ca-edit")).click();
-//
-//      // verify edit page UI
-//      Assert.assertEquals("http://en.wikipedia.org/w/index.php?title=Ultimate_Fighting_Championship&action=edit", driver.getCurrentUrl());
-//      Assert.assertTrue(driver.findElement(By.cssSelector(".wikiEditor-ui")).isDisplayed());
-//    }
+
+    @Test
+    public void verifyEditPageUI() throws Exception {
+    	driver.get("http://en.wikipedia.org/wiki/Ultimate_Fighting_Championship");
+
+    	// click edit page
+    	driver.findElement(By.cssSelector("#ca-edit a")).click();
+
+    	// verify edit page UI
+    	Assert.assertEquals("http://en.wikipedia.org/w/index.php?title=Ultimate_Fighting_Championship&action=edit", driver.getCurrentUrl());
+    	Assert.assertTrue(driver.findElement(By.cssSelector(".wikiEditor-ui")).isDisplayed());
+    }
 }
