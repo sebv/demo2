161c1cf382a80087abe2368fce30118fc6ceb91c
Neil <neil@Neils-MacBook-Pro.local>
2015-04-15 18:32:31 -0700

    Test file name needs to start with "Test"

--- src/test/java/com/saucelabs/TestNGWikipediaDemo.java
+++ src/test/java/com/saucelabs/TestNGWikipediaDemo.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.By;
 
-public class WikipedaTestNGExample {
+public class TestNGWikipediaDemo {
 
     private WebDriver driver;
 
